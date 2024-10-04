089581ccbdd2d98cb1162af15cc272f1de3dcf47
Kris Qikun Li - Mac <qikunli8866+mac@gmail.com>
2024-10-04 19:13:29 +1300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function MyButton() {
+    return <button className="Mybutton1">Click me!</button>;
+}
+
+function App() {
+    return (
+        <>
+            <h1>hello, world!</h1>
+            <MyButton />
+        </>
+    );
+}
+export default App;
